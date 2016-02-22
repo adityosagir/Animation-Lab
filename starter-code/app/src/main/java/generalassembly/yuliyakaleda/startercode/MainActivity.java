05ac52390999357582636b68c553c37bc911e3b5
adityosagir <adityo.sagir@gmail.com>
2016-02-21 20:58:39 -0500

    Adityo - Animation-Lab

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -5,23 +5,63 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    //TODO: set up all the view and event listeners.
-  }
+        //TODO: set up all the view and event listeners.
 
-  @Override
-  public void onClick(View v) {
-    // TODO: 1. get the text from the input field
-    //       2. animate it in the center of the screen
-    //       3. add it to the list wish
-    //       4. clear the input field
-  }
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 1. get the text from the input field
+                //       2. animate it in the center of the screen
+                //       3. add it to the list wish
+                //       4. clear the input field
+
+                textView.setText(editText.getText().toString());
+
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
+                textView.startAnimation(animation);
+
+                String input = editText.getText().toString();
+                list.add(input);
+
+                editText.getText().clear();
+
+                // Remove wish from ArrayList
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        list.remove(position);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+            }
+        });
+    }
+
 }
